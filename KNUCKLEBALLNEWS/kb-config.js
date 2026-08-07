c7f3128ceb4ec110e9690b5d35939cf43605ad0c
dlong6 <devinlong618@gmail.com>
2026-08-07 15:40:27 -0700

    Delete KNUCKLEBALLNEWS directory

--- KNUCKLEBALLNEWS/kb-config.js
+++
@@ -1,5 +0,0 @@
-window.KB_CONFIG = {
-  // Fill these from your Supabase project settings.
-  supabaseUrl: "https://yzhcqmodzkbvydyybitf.supabase.co",
-  supabaseAnonKey: "sb_publishable_LIEkc6h5VluTYSKtw2BNmw_fTNzDSgG",
-};
